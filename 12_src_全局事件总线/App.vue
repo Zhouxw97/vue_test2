c1c22c814ba2c841a3078e4c9daf26c72d16acc2
zhouxiaowen <00441208@want-want.com>
2022-03-29 16:40:52 +0800

    全局事件总线

---
+++ 12_src_全局事件总线/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <h1 v-text="msg" ref="title"></h1>
+      <button @click="showDOM">点我输出上方DOM元素</button>
+      <School/>
+      <Student/>
+
+  </div>
+</template>
+
+<script>
+    import School from './components/School'
+    import Student from './components/Student'
+
+    export default {
+
+        name:'App',
+        components:{
+            School,
+            Student
+        },
+        data(){
+            return {
+                msg:'欢迎学习Vue!'
+            }
+        },
+        methods:{
+            showDOM(){
+                console.log(this.$refs.title)
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
